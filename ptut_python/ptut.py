403d562f2b46af335ab48f97d3c3c0417d6eb35c
p2103031 <jules.valette2@etu.univ-lyon1.fr>
2022-12-09 15:18:58 +0100

    Add shared memory handler

--- ptut_python/ptut.py
+++ ptut_python/ptut.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import mmap
 from mediapipe.framework.formats import landmark_pb2
 
 mp_drawing = mp.solutions.drawing_utils
@@ -7,13 +8,14 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
 def __main__() :
+    shmem = mmap.mmap(-1, 16*4 ,"shm", access=mmap.ACCESS_WRITE)
     cap = cv2.VideoCapture(0)
     with mp_pose.Pose(
         min_detection_confidence=0.7,
         min_tracking_confidence=0.7) as pose:
-        
+        tab = [[0]*8,[0]*8]
         while cap.isOpened():
-            success, image = cap.read()
+            success, image = cap.read(0)
             if not success:
                 print("Ignoring empty camera frame.")
             # If loading a video, use 'break' instead of 'continue'.
@@ -43,16 +45,27 @@ def __main__() :
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
                 poses=landmark_subset.landmark
                 cpt = 0
+                
                 for data_point in poses:
                     x=data_point.x * 640
                     y=data_point.y * 480
-                    cpt+=1
+                    tab[0][cpt] = int(x)
+                    tab[1][cpt] = int(y)
+                    """
                     if(cpt %2 == 0):
-                        cv2.circle(image,(int(x),int(y)),10,(255,0,0),-1)
+                        cv2.circle(image,(tab[0][cpt],tab[1][cpt]),10,(255,0,0),1)
                     else:
-                        cv2.circle(image,(int(x),int(y)),10,(0,0,255),-1)
+                        cv2.circle(image,(tab[0][cpt],tab[1][cpt]),10,(0,0,255),1)
+                    """
+                    cpt+=1
+                cpt = 0
+                for op in tab:
+                    for data in op:
+                        shmem.seek(cpt)
+                        shmem.write((data).to_bytes(4,"little"))
+                        cpt += 4 
+                shmem.read()
             # Flip the image horizontally for a selfie-view display.
-            cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
             if cv2.waitKey(5) & 0xFF == 27:
                 break
         cap.release()
